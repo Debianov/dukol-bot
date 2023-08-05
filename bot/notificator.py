2d93d7e2c31f89814fffdb8c3f4bd65e4eeff5dd
Debianov <65801480+Debianov@users.noreply.github.com>
2023-08-05 17:16:28 +0000

    Упрощение кода и очистка избытков

--- bot/notificator.py
+++ bot/notificator.py
@@ -9,30 +9,23 @@ class UpdateNotificator(commands.Cog):
   
   def __init__(self, bot: commands.Bot, channel_id: int) -> None:
     self.bot = bot
-    self.channel = self.bot.get_channel(channel_id)
-    self.new_part_msg: str = ""
+    self.target_channel = self.bot.get_channel(channel_id)
     self.len_last_msg: int = -1
 
   async def cog_load(self) -> None:
     self.update_checker.start()
 
-  @tasks.loop(seconds=1.0)
+  @tasks.loop(seconds=5.0)
   async def update_checker(self) -> None:
     msg_to_analyze = await steam.gather_data("516750", "3069740688714545717")
-    self.new_part_msg = msg_to_analyze
-    await self.send_update_notification()
-    if self.len_last_msg == -1:
-        self.len_last_msg = len(msg_to_analyze)
-    else:
-      if self.len_last_msg < len(msg_to_analyze):
-        self.new_part_msg = msg_to_analyze[self.len_last_msg:]
+    if self.len_last_msg < len(msg_to_analyze):
+      await self.send_update_notification(msg_to_analyze[self.len_last_msg:])
+    self.len_last_msg = len(msg_to_analyze)
   
-  async def send_update_notification(self) -> None:
-    if self.new_part_msg:
-      notification_embed = Embed()
-      notification_embed.add_field(name="повідомлення", value=self.new_part_msg)
-      await self.target_channel.send(embed=notification_embed)
-      self.new_part_msg = ""
+  async def send_update_notification(self, msg_text: str) -> None:
+    notification_embed = Embed()
+    notification_embed.add_field(name="повідомлення", value=msg_text)
+    await self.target_channel.send(embed=notification_embed)
 
 async def setup(bot):
   target_channel = 792679070008606753
